af59e43a07dcc20503224b4d236da02d6000af7e
William Staniscia <william.staniscia@outlook.com>
2019-10-17 22:28:13 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,5 +1,5 @@
 def greeting(bill)
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+  puts "Hi, #{bill}! Welcome to the wonderful world of Ruby programming."
   puts "Please enter your name so that we can greet you more personally:"
   name = gets.strip
   greeting(name)
