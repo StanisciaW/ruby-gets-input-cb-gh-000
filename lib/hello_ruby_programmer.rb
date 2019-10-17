2bc52e5c69f6970131e6287341bcc07f51885657
William Staniscia <william.staniscia@outlook.com>
2019-10-17 22:30:52 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,4 +1,6 @@
-puts "Hi! Welcome to the wonderful world of Ruby programming."
-puts "Please enter your name so that we can greet you more personally:"
-name = gets.strip
-greeting(name)
+def greeting
+  puts "Hi, #{bill}! Welcome to the wonderful world of Ruby programming."
+  puts "Please enter your name so that we can greet you more personally:"
+  name = gets.strip
+  greeting(name)
+end
